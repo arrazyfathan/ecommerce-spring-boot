0a4be7365790faf0d5ccd0332a0e16633237219b
arrazyfathan <arrazy.rabbani266@gmail.com>
2024-12-13 16:01:59 +0700

    Add user service and user controller

--- src/main/java/com/arrazyfathan/ecommerce/controller/UserController.java
+++ src/main/java/com/arrazyfathan/ecommerce/controller/UserController.java
@@ -1,19 +1,24 @@
 package com.arrazyfathan.ecommerce.controller;
 
 
+import com.arrazyfathan.ecommerce.data.entity.UserEntity;
 import com.arrazyfathan.ecommerce.data.model.RegisterRequest;
 import com.arrazyfathan.ecommerce.data.response.BaseResponse;
 import com.arrazyfathan.ecommerce.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/user")
 @AllArgsConstructor
 public class UserController {
 
+    private UserService userService;
+
+    @GetMapping
+    public BaseResponse getUser() {
+        UserEntity userEntity = userService.userEntity();
+        return new BaseResponse(true, "Success", userEntity);
+    }
 
 }
--- src/main/java/com/arrazyfathan/ecommerce/service/UserService.java
+++ src/main/java/com/arrazyfathan/ecommerce/service/UserService.java
@@ -1,6 +1,10 @@
 package com.arrazyfathan.ecommerce.service;
 
+import com.arrazyfathan.ecommerce.data.entity.UserEntity;
+import com.arrazyfathan.ecommerce.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,5 +13,16 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public UserEntity userEntity() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        String phoneNumber = userDetails.getUsername();
+        return userRepository.findByPhoneNumber(phoneNumber)
+                .orElseThrow();
+    }
 
 }
